4efb3b9977739cc81041b503a19a7f08c3b06c80
Yockium <mtfmattye@gmail.com>
2024-08-01 13:18:31 +0200

    task interaction such as remove task, complete task

--- src/main.js
+++ src/main.js
@@ -1,16 +1,52 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function() {
     const button = document.getElementById("button");
     const input = document.getElementById("input");
     const list = document.getElementById("list");
 
     const taskList = [];
-    button.addEventListener("click", function(event) {
-        addTask(input.value);
+    button.addEventListener("click", function() {
+        if (input.value.trim() === "") {
+            return;
+        }
+        taskList.push(input.value);
+        renderTasks();
         console.log(taskList);
     });
 
-    function addTask(value) {
-        taskList.push(value);
+    function renderTasks() {
+        list.innerHTML = '';
+
+        taskList.forEach((task, index) => {
+            const taskItem = document.createElement("li");
+            taskItem.classList.add("taskItem");
+            taskItem.id = `taskId-${index}`;
+            taskItem.innerText = task;
+            list.appendChild(taskItem);
+
+            const removeButton = document.createElement("button");
+            removeButton.classList.add("remove");
+            removeButton.innerText = "X";
+            removeButton.addEventListener("click", function() {
+                taskList.splice(index, 1);
+                renderTasks();
+            });
+            taskItem.appendChild(removeButton);
+
+            const completeButton = document.createElement("button");
+            completeButton.classList.add("complete");
+            completeButton.innerText = "complete";
+            completeButton.addEventListener("click", function() {
+                taskItem.classList.toggle("completed");
+                if (completeButton.innerText === "complete") {
+                    completeButton.innerText = "completed";
+                }
+                else {
+                    completeButton.innerText = "complete"
+                }
+            })
+            taskItem.appendChild(completeButton);
+        })
+
     }
 
     console.log(taskList);
